082f7dd75e912e7389144f53d12a4e21c6f3f095
Mostafa Rezaie <mostafarezaie.500@gmail.com>
2025-09-09 10:11:20 +0430

    fix: issues

--- resources/js/Pages/ActivityLogs/Index.jsx
+++ resources/js/Pages/ActivityLogs/Index.jsx
@@ -12,11 +12,11 @@ import RoundedButton from '@/Components/ui/RoundedButton';
 import { onConfirm } from '@/lib/appAlert';
 import ButtonLink from '@/Components/ui/form/ButtonLink';
 import SingleShow from './SingleShow';
-import { hasPermission } from '../../hooks/usePermission';
+import { usePermission } from '@/hooks/usePermission';
 import moment from 'moment';
 
 const Index = ({ columns }) => {
-    const { hasPermission } = hasPermission();
+    const { hasPermission } = usePermission();
     const { t } = useTranslation();
     const route = window.route;
     const [refreshDatatable, setRefreshDatatable] = useState(false);
